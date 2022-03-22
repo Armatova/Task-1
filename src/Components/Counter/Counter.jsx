44749bf4e338e94aec40e5eaf4afbe63d09b06d7
Armatova <armatovaaizusha@gmail.com>
2022-03-22 09:57:14 +0600

    First commit

---
+++ src/Components/Counter/Counter.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+
+const Counter = () => {
+  const [counter, setCounter] = useState(0);
+
+  function increment() {
+    setCounter(counter + 1);
+  }
+  function decrement() {
+    setCounter(counter - 1);
+  }
+
+  return (
+    <div>
+      <h1>Счетчик:{counter}</h1>
+      <button onClick={increment} className="btn btn-success">
+        +
+      </button>
+      <button onClick={decrement} className="btn btn-warning">
+        -
+      </button>
+    </div>
+  );
+};
+
+export default Counter;
